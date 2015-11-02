697c7b5b2e16ce23baeefce8230a7e328c5bd6a3
Howard <howarde.jr@hotmail.com>
2015-11-02 13:53:06 -0500

    update

--- assignment2.pl
+++ assignment2.pl
@@ -1,4 +1,18 @@
-goal_state([clarendon/green, hanover/red, kingston/yellow, manchester/blue, portland/blue, stAndrew/red, stAnn/red, stCatherine/blue, stElizabeth/red, stJames/blue, stMary/yellow, stThomas/yellow, trelawny/yellow, westmoreland/yellow]).
+goal_state([clarendon/green, hanover/red, kingston/yellow,
+	    manchester/blue, portland/blue, stAndrew/red,
+	    stAnn/red, stCatherine/blue, stElizabeth/red,
+	    stJames/blue, stMary/yellow, stThomas/yellow,
+	    trelawny/yellow, westmoreland/yellow]).
+
+dfs(S, Path, Path) :- goal_state(S).
+
+dfs(S, Checked, Path) :-
+    % try a move
+    move(S, S2),
+    % ensure the resulting state is new
+    \+member(S2, Checked),
+    % and that this state leads to the goal
+    dfs(S2, [S2|Checked], Path).
 
 color(green).
 color(red).
@@ -34,6 +48,56 @@ adjacentParishes(stAndrew, [stCatherine,stMary,kingston,portland,stThomas]).
 adjacentParishes(kingston, [stAndrew]).
 adjacentParishes(portland, [stMary,stAndrew,stThomas]).
 adjacentParishes(stThomas, [stAndrew,portland]).
+
+move(hanover,westmoreland).
+move(hanover,stJames).
+move(westmoreland,hanover).
+move(westmoreland,stJames).
+move(westmoreland,stElizabeth).
+move(stJames,hanover).
+move(stJames,westmoreland).
+move(stJames,trelawny).
+move(stElizabeth,westmoreland).
+move(stElizabeth,stJames).
+move(stElizabeth,trelawny).
+move(stElizabeth,manchester).
+move(trelawny,stJames).
+move(trelawny,stElizabeth).
+move(trelawny,manchester).
+move(trelawny,stAnn).
+move(trelawny,clarendon).
+move(manchester,stElizabeth).
+move(manchester,trelawny).
+move(manchester,stAnn).
+move(manchester,clarendon).
+move(stAnn,trelawny).
+move(stAnn,manchester).
+move(stAnn,clarendon).
+move(stAnn,stCatherine).
+move(stAnn,stMary).
+move(clarendon,trelawny).
+move(clarendon,manchester).
+move(clarendon,stAnn).
+move(clarendon,stCatherine).
+move(stCatherine,stAnn).
+move(stCatherine,clarendon).
+move(stCatherine,stMary).
+move(stCatherine,stAndrew).
+move(stMary,stAnn).
+move(stMary,stCatherine).
+move(stMary,stAndrew).
+move(stMary,portland).
+move(stAndrew,stCatherine).
+move(stAndrew,stMary).
+move(stAndrew,kingston).
+move(stAndrew,portland).
+move(stAndrew,stThomas).
+move(kingston,stAndrew).
+move(portland,stMary).
+move(portland,stAndrew).
+move(portland,stThomas).
+move(portland,stAndrew).
+move(portland,stThomas).
 
 adjacent(Parish, OtherParish):-
         adjacentParishes(Parish, AdjacentParish),
